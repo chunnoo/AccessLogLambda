1da7fa813bc5754fd588662d9a41de39e5e1c74b
Mathias Chunnoo <mchunnoo@verizonmedia.com>
2021-06-24 11:51:12 +0200

    add query extraction

--- access-log/app.js
+++ access-log/app.js
@@ -1,49 +1,69 @@
 const aws = require("aws-sdk");
-const s3 = new aws.S3({ apiVersion: "2006-03-01"});
+const s3 = new aws.S3({ apiVersion: "2006-03-01" });
 const { spawn } = require("child_process");
 
 const findRelevantKeys = ({ Bucket }) => {
   console.log(`Finding relevant keys in bucket ${Bucket}`);
-  return s3.listObjectsV2({ Bucket }).promise()
-    .then(res => res.Contents
-      .map(content => ({ Bucket, Key: content.Key }))
-      .filter(obj => /JsonAccessLog\.default\.\d+\.zst/.test(obj.Key)))
-    .catch(err => Error(err));
+  return s3
+    .listObjectsV2({ Bucket })
+    .promise()
+    .then((res) =>
+      res.Contents.map((content) => ({ Bucket, Key: content.Key })).filter(
+        (obj) => /JsonAccessLog\.default\.\d+\.zst/.test(obj.Key)
+      )
+    )
+    .catch((err) => Error(err));
 };
 
 const getObjectData = ({ Bucket, Key }) => {
   console.log(`Getting object with key ${Key} from bucket ${Bucket}`);
-  return s3.getObject({ Bucket, Key }).promise()
-    .then(res => res.Body)
-    .catch(err => Error(err));
+  return s3
+    .getObject({ Bucket, Key })
+    .promise()
+    .then((res) => res.Body)
+    .catch((err) => Error(err));
 };
 
-const decompress = (buffer) => new Promise((resolve, reject) => {
-  const zstd = spawn("zstd", ["-cd"]);
+const decompress = (buffer) =>
+  new Promise((resolve, reject) => {
+    const zstd = spawn("zstd", ["-cd"]);
 
-  zstd.stdout.on("data", data => {
-    resolve(data.toString());
+    zstd.stdout.on("data", (data) => {
+      resolve(data.toString());
+    });
+
+    zstd.stderr.on("data", (err) => {
+      reject(err);
+    });
+
+    zstd.stdin.write(buffer);
+    zstd.stdin.end();
   });
 
-  zstd.stderr.on("data", err => {
-    reject(err);
-  });
-
-  zstd.stdin.write(buffer);
-  zstd.stdin.end();
-});
+const extractQuery = (logFile) => {
+  return logFile
+    .split(/(?:\r\n|\r|\n)/g)
+    .map((line) => line.match(/input=(.*)&jsoncallback/))
+    .filter((match) => match !== null)
+    .map((match) => match[1])
+    .map((str) => str.replace(/%22/g, '"'))
+    .map((str) => str.replace(/%5C/g, "\\"))
+    .map((str) => str.replace(/%08|%09|%0A|%0B|%0C/g, ""))
+    .map((str) => decodeURIComponent(str))
+    .flat();
+};
 
 exports.handler = async (event, context) => {
   const options = { Bucket: "chunnoo-test-bucket" };
 
   return findRelevantKeys(options)
-    .then(objs => Promise.all(
-      objs.map(obj => getObjectData(obj))))
-    .then(buffers => Promise.all(
-      buffers.map(buffer => decompress(buffer))))
-    .then(res => {
-      console.log("response: ", res);
-      return {statusCode: 200};
+    .then((objs) => Promise.all(objs.map((obj) => getObjectData(obj))))
+    .then((buffers) => Promise.all(buffers.map((buffer) => decompress(buffer))))
+    .then((logFiles) => logFiles.map((logFile) => extractQuery(logFile)))
+    .then((res) => {
+      console.log("responses: ");
+      res.forEach((query) => console.log(query));
+      return { statusCode: 200 };
     })
-    .catch(err => ({statusCode: 500, message: err}));
+    .catch((err) => ({ statusCode: 500, message: err }));
 };
